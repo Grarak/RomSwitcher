243b1cc90ba8454b2619fa9378c5b9c1ba673eb7
Willi Ye <williye97@gmail.com>
2014-04-05 22:36:27 +0200

    license as Apache 2.0

--- app/src/main/java/com/grarak/romswitcher/activities/RomSwitcherActivity.java
+++ app/src/main/java/com/grarak/romswitcher/activities/RomSwitcherActivity.java
@@ -1,9 +1,5 @@
 package com.grarak.romswitcher.activities;
 
-/**
- * Created by grarak's kitten (meow) on 31.03.14.
- */
-
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
@@ -25,6 +21,26 @@ import com.stericson.RootTools.RootTools;
 
 import java.util.ArrayList;
 import java.util.List;
+
+/*
+ * Copyright (C) 2013 The RomSwitcher Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Created by grarak's kitten (meow) on 31.03.14.
+ */
 
 public class RomSwitcherActivity extends Activity implements ActionBar.TabListener, Constants {
 
